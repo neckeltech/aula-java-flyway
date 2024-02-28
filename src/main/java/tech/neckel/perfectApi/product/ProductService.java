eefa9cd1883917d73541d673820173af3f638f72
Andre Neckel <andre.neckel@viasoft.com.br>
2024-02-28 19:05:11 -0300

    Initial commit.

---
+++ src/main/java/tech/neckel/perfectApi/product/ProductService.java
@@ -0,0 +1,10 @@
+package tech.neckel.perfectApi.product;
+
+import java.util.List;
+
+public interface ProductService {
+    List<Product> listAll();
+    Product create(Product product);
+    Product update(Product product);
+    void delete(Long id);
+}
